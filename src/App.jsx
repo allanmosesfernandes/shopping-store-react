fe6accdeb867009ea0abb45f41c2a67e2845ea77
Allan Fernandes <Fernzallan14@gmail.com>
2022-11-30 08:28:33 +0000

    categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+]
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='categories-container'>
+      { 
+        categories.map((category) => {(
+          <div className='category-container'>
+            <h2>`${category.title}`</h2>
+          </div>
+        )}) 
+      }
     </div>
   )
 }
